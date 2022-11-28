cc20517a04520891ebd37ea19c81b3644245b73f
krys-kala <101945857+krys-kala@users.noreply.github.com>
2022-11-28 18:14:10 -0500

    Test Commit

---
+++ server/index.js
@@ -0,0 +1,6 @@
+// Using the new way of importing the modules as what the newest NodeJS supports
+// For this to work, "type" must be declared in the package.json file
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import mongoose from 'mongoose';
