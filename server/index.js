bff4325836e2ab6aac269bc599d1d2538e9e36b2
krys-kala <101945857+krys-kala@users.noreply.github.com>
2022-11-28 19:02:36 -0500

    Server Setup Completed

--- server/index.js
+++ server/index.js
@@ -4,3 +4,22 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import mongoose from 'mongoose';
+
+import postRoutes from './routes/posts.js';
+
+const app = express();
+
+app.use('/posts', postRoutes);      // allows the website to start all the routes from the posts.js 
+
+app.use(bodyParser.json({ limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}));
+app.use(cors());
+
+const connection_url = 'mongodb+srv://admin:adminadmin@assignment-3.pant6jn.mongodb.net/?retryWrites=true&w=majority'
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(connection_url, { useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+    .catch((error) => console.log(error.message));
+
+// mongoose.set('useFindAndModify', false);     <- no longer needed
