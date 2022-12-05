5e16488e317d83a1f9598ab01d8cb3603859e9f6
Yang Chen <yang.chen1@ontariotechu.net>
2022-12-04 20:58:39 -0500

    Navbar

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import memories from './images/memories.jpg';
 import Posts from './components/Posts/Posts.js';
 import Form from './components/Form/Form.js';
 import useStyles from './styles.js';
+import Home from './components/Home/Home';
 
 const App = () => {
     const classes = useStyles();
@@ -13,20 +14,7 @@ const App = () => {
                 <Typography className={classes.heading} variant="h2" align="center">Memories</Typography>
                 <img className={classes.image} src={memories} alt="memories" height="60"/>
             </AppBar>
-            <Grow in>
-                <Container>
-                    <Grid contianer justify="space-between" alignItems="stretch" spacing={3}>
-                        <Grid item xs={12} sm={7}>
-                            <Posts />
-
-                        </Grid>
-                        <Grid item xs={12} sm={4}>
-                            <Form />
-
-                        </Grid>
-                    </Grid>
-                </Container>
-            </Grow>
+            <Home />
         </Container>
     );
 }
